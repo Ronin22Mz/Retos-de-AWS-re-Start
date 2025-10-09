1fc51f1a9ff86883798541983b0034893e45cdc2
Harol Benjamin Medina Zárate <benja15mz@gmail.com>
2025-10-08 19:51:22 -0500

    Actualizar

---
+++ Reto2.py
@@ -0,0 +1,27 @@
+# Ejercicio 1: Mostrar tipos de datos numéricos
+print("Python tiene tres tipos numericos: int, float y complex")
+
+Entero = 1
+print(Entero)
+print(type(Entero))
+print(str(Entero) + " es del tipo de dato " + str(type(Entero)))
+
+Flotante = 3.14
+print(Flotante)
+print(type(Flotante))
+print(str(Flotante) + " es del tipo de dato " + str(type(Flotante)))
+
+miValor = 5j
+print(miValor)
+print(type(miValor))
+print(str(miValor) + " es del tipo de dato " + str(type(miValor)))
+
+Boleano = True
+print(Boleano)
+print(type(Boleano))
+print(str(Boleano) + " es del tipo de dato " + str(type(Boleano)))
+
+Cadena = "Hola"
+print(Cadena)
+print(type(Cadena))
+print(str(Cadena) + " es del tipo de dato " + str(type(Cadena)))
